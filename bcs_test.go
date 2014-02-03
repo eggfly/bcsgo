351d1dd4b6dbc0e3048575005c424ee8894309e3
eggfly <lihaohua90@gmail.com>
2014-02-04 01:39:41 +0800

    create file with dir, todos and tips
    Signed-off-by: eggfly <lihaohua90@gmail.com>

--- bcs_test.go
+++ bcs_test.go
@@ -10,11 +10,18 @@ func Test(t *testing.T) {
 	url := bcs.Sign("GET", "", "/", "", "", "")
 	url_ex := "http://bcs.duapp.com//?sign=MBO:vYlphQiwbhVz67jjW48ddY3C:yf27Oy6JVtK6nxRtIASKX6H%2BR4I%3D"
 	fmt.Println("test sign", url == url_ex)
+
 	newBucket := bcs.Bucket("testsml2")
 	bucketErr := newBucket.Create()
 	fmt.Println(bucketErr)
+
 	bucketErr = newBucket.Delete()
 	fmt.Println(bucketErr)
+
+	newBucket2 := bcs.Bucket("testErrorBucket")
+	bucketErr2 := newBucket2.Create()
+	fmt.Println(bucketErr2)
+
 	b, e := bcs.ListBuckets()
 	fmt.Println(e)
 	// for _, pBucket := range b {
@@ -26,9 +33,10 @@ func Test(t *testing.T) {
 	// 	}
 	// }
 
-	testObj := b[0].Object("/test.txt")
+	testObj := b[0].Object("/testDir/test.txt")
 	testObj, err := testObj.PutFile("test.txt")
 	fmt.Println(testObj, err)
+
 	deleteErr := testObj.Delete()
 	fmt.Println(deleteErr)
 }
